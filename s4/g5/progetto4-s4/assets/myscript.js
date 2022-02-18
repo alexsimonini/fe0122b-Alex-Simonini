8a679cdd5b741a85443a4ae5ddd3a2402218f695
fe0122b-Alex-Simonini <simoninialex2002@gmail.com>
2022-02-18 14:15:42 +0100

    progetto4aggiornato

--- s4/g5/progetto4-s4/assets/myscript.js
+++ s4/g5/progetto4-s4/assets/myscript.js
@@ -1,20 +1,25 @@
-function prendiDato(pippo){
-    let display = document.getElementById('display')
-    let numero = pippo.id
-    let attuaValore = display.value;
-    
-        display.value += numero
+var operazioneCliccata = false
 
-   
+function prendiDato (valore) {
+    if (valore == '+' || valore == '-' || valore == '/' || valore == '*'){
+        if (operazioneCliccata == false) {
+            document.getElementById('display').value += valore;
+            operazioneCliccata = true;
+        }
+    }
+    if (Number.isInteger(valore) || valore =='.') {
+        document.getElementById ('display').value += valore;
+    }
 }
 
 function resetCalcolatrice(){
     let display = document.getElementById('display').value = '';
+    operazioneCliccata = false
 }
 
 
 function totale(){
     let display = document.getElementById('display')
     display.value = eval(display.value);
+    operazioneCliccata = false
 }
-
