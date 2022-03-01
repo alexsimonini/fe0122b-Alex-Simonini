191c7cbaa56612875ed314857a169540efa1bca9
fe0122b-Alex-Simonini <simoninialex2002@gmail.com>
2022-03-01 17:28:55 +0100

    esercizios6g2

---
+++ s6/g2/esercizios6/FAQ-finale-solo-indicazioni-1/script.js
@@ -0,0 +1,12 @@
+
+    $('#h2').on('click',function(){
+        $('#div').toggle('display')
+    })
+
+    $('#h3').on('click',function(){
+        $('#div1').toggle('display')
+    })
+
+    $('#h4').on('click',function(){
+        $('.div2').toggle('display')
+    })
